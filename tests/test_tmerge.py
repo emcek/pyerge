7ae33b24558ed3ffda61dc3b66916493b9558d7c
Michal Plichta <mplichta@gmail.com>
2019-04-28 23:24:02 +0200

    add 3 tests form tmerge

---
+++ tests/test_tmerge.py
@@ -0,0 +1,28 @@
+from unittest import mock
+
+from pytest import mark
+
+from pyerge import tmerge
+
+
+@mock.patch('pyerge.tmerge.utils')
+def test_is_portage_running(utils_mock):
+    utils_mock.run_cmd.return_value = (b'3456\n', b'')
+    assert tmerge.is_portage_running() is True
+    utils_mock.run_cmd.assert_called_once_with('pgrep -f /usr/bin/emerge')
+
+
+@mock.patch('pyerge.tmerge.utils')
+def test_is_portage_not_running(utils_mock):
+    utils_mock.run_cmd.return_value = (b'', b'')
+    assert tmerge.is_portage_running() is False
+    utils_mock.run_cmd.assert_called_once_with('pgrep -f /usr/bin/emerge')
+
+
+@mark.parametrize('list_str, result', [(['-pvNDu', '@world'], (True, True)),
+                                       (['-pv', 'conky'], (True, False)),
+                                       (['-f', 'conky'], (False, False)),
+                                       (['', '@world'], (False, True)),
+                                       (['', 'conky'], (False, False))])
+def test_check_emerge_opts(list_str, result):
+    assert tmerge.check_emerge_opts(list_str) == result
