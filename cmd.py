cc3b588531b564354eaf3bf7abb4634887a400fc
Michal Plichta <emcek@users.noreply.github.com>
2019-04-29 14:06:45 +0200

    add draft of cmd

---
+++ cmd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.6
+"""Various tools to emerge and to show status for conky."""
+from argparse import ArgumentParser
+
+
+def run():
+    """Function to collect command line arguments and construct main object with correct set of parameters."""
+    parser = ArgumentParser(description='Pyerge')
+
+
+if __name__ == '__main__':
+    run()
