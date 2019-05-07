2030ba7725981cdb88f14bcae2c09fbe4856188c
Michal Plichta <michal.plichta@nokia.com>
2019-05-07 22:01:00 +0200

    extract functions

--- pyerge/cli.py
+++ pyerge/cli.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3.6
 """Various tools to emerge and to show status for conky."""
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 from logging import basicConfig, warning, DEBUG, info, error
+from typing import List
 
 from pyerge import tmerge, utils, __version__, portage_tmpdir
 
@@ -42,28 +43,55 @@ def run_parser():
     utils.set_portage_tmpdir()
 
     if not tmerge.is_portage_running():
-        if not utils.is_tmpfs_mounted(portage_tmpdir):
-            utils.mounttmpfs(opts.size, opts.verbose, portage_tmpdir)
-        elif utils.size_of_mounted_tmpfs(portage_tmpdir) != utils.convert2blocks(opts.size):
-            utils.remounttmpfs(opts.size, opts.verbose, portage_tmpdir)
-        else:
-            if opts.verbose:
-                info('tmpfs is already mounted with requested size!')
-
-        if opts.action == 'emerge':
-            ret_code = tmerge.emerge(emerge_opts, opts.verbose, build=True)
-            tmerge.post_emerge(emerge_opts, opts.verbose, ret_code)
-            if opts.deep:
-                tmerge.deep_clean(emerge_opts, opts.verbose, ret_code)
-        elif opts.action == 'check':
-            if utils.is_internet_connected() or opts.local:
-                if opts.verbose:
-                    info('There is internet connecton')
-                tmerge.check_upd(opts.local, opts.verbose)
-            else:
-                if opts.verbose:
-                    warning('No internet connection!\n')
+        handling_mounting(opts)
+        run_emerge(emerge_opts, opts)
+        run_check(opts)
     else:
         if opts.verbose:
             info('emerge already running!')
     utils.unmounttmpfs(opts.size, opts.verbose, portage_tmpdir)
+
+
+def handling_mounting(opts: Namespace) -> None:
+    """
+    Handling mounting temporary file fistem with requestes size.
+
+    :param opts: cli arguments
+    """
+    if not utils.is_tmpfs_mounted(portage_tmpdir):
+        utils.mounttmpfs(opts.size, opts.verbose, portage_tmpdir)
+    elif utils.size_of_mounted_tmpfs(portage_tmpdir) != utils.convert2blocks(opts.size):
+        utils.remounttmpfs(opts.size, opts.verbose, portage_tmpdir)
+    else:
+        if opts.verbose:
+            info('tmpfs is already mounted with requested size!')
+
+
+def run_emerge(emerge_opts: List[str], opts: Namespace) -> None:
+    """
+    Run update of system.
+
+    :param emerge_opts: list of arguments for emege
+    :param opts: cli arguments
+    """
+    if opts.action == 'emerge':
+        ret_code = tmerge.emerge(emerge_opts, opts.verbose, build=True)
+        tmerge.post_emerge(emerge_opts, opts.verbose, ret_code)
+        if opts.deep:
+            tmerge.deep_clean(emerge_opts, opts.verbose, ret_code)
+
+
+def run_check(opts: Namespace) -> None:
+    """
+    Run checking system updates.
+
+    :param opts: cli arguments
+    """
+    if opts.action == 'check':
+        if utils.is_internet_connected() or opts.local:
+            if opts.verbose:
+                info('There is internet connecton')
+            tmerge.check_upd(opts.local, opts.verbose)
+        else:
+            if opts.verbose:
+                warning('No internet connection!\n')
