7a014e2b8db2da08f75086388a858d250df8cbd2
Michal Plichta <michal.plichta@nokia.com>
2019-05-11 17:39:20 +0200

    add test only form python 3.7+

--- tests/test_file_utils.py
+++ tests/test_file_utils.py
@@ -1,27 +1,24 @@
+from sys import version_info
 from unittest import mock
 from unittest.mock import mock_open, Mock
 
+from pytest import mark
+
 from pyerge.file_utils import e_sync
 
-list_no_sync = ["1548194358:  === sync",
-                "1548194358: >>> Syncing repository 'gentoo' into '/usr/portage'...",
-                "1548194361: >>> Starting rsync with rsync://[2a01:90:200:10::1a]/gentoo-portage"]
+str_no_sync = """1548194358:  === sync
+1548194358: >>> Syncing repository 'gentoo' into '/usr/portage'...
+1548194361: >>> Starting rsync with rsync://[2a01:90:200:10::1a]/gentoo-portage"""
 
-list_sync = ["1548194361: >>> Starting rsync with rsync://[2a01:90:200:10::1a]/gentoo-portage",
-             "1548194466: === Sync completed for gentoo",
-             "1548194466:  *** terminating."]
-
-str_sync = """
-1548194361: >>> Starting rsync with rsync://[2a01:90:200:10::1a]/gentoo-portage
+str_sync = """1548194361: >>> Starting rsync with rsync://[2a01:90:200:10::1a]/gentoo-portage
 1548194466: === Sync completed for gentoo
-1548194466:  *** terminating.
-"""
+1548194466:  *** terminating."""
 
 
 def test_synced():
     with mock.patch('pyerge.file_utils.open') as open_mock:
         open_mock.return_value.__enter__ = open_mock
-        open_mock.return_value.__iter__ = Mock(return_value=iter(list_sync))
+        open_mock.return_value.__iter__ = Mock(return_value=iter(list(str_sync.split('\n')))
         assert e_sync() == 'Tuesday 22:01' or 'Tuesday 23:01'
         # open_mock.assert_called_once_with('/var/log/emerge.log')
 
@@ -29,20 +26,15 @@ def test_synced():
 def test_no_synced():
     with mock.patch('pyerge.file_utils.open') as open_mock:
         open_mock.return_value.__enter__ = open_mock
-        open_mock.return_value.__iter__ = Mock(return_value=iter(list_no_sync))
+        open_mock.return_value.__iter__ = Mock(return_value=iter(list(str_no_sync.split('\n'))))
         assert e_sync() == 'Unknown'
         # open_mock.assert_called_once_with('/var/log/emerge.log')
 
 
-def test_synced_2():
-    # Not working shoud faild
+@mark.skipif(condition=version_info < (3, 7), reason='Run only on Python 3.7+')
+def test_synced_ver2():
     with mock.patch('pyerge.file_utils.open', mock_open(read_data=str_sync)) as m:
-        # m.side_effect = [
-        #     mock_open(read_data="1548194466:  *** terminating.").return_value,
-        #     mock_open(read_data="1548194466: === Sync completed for gentoo").return_value
-        # ]
         result = e_sync()
 
     m.assert_called_once_with('/var/log/emerge.log')
-    assert result == 'Unknown'
-    # assert result == 'Tuesday 23:01'
+    assert result == 'Tuesday 22:01' or 'Tuesday 23:01'
