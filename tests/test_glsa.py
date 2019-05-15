d35656fd88d53ec2abacfdb865e4ad7afacbaa6d
Michal Plichta <michal.plichta@nokia.com>
2019-05-15 14:51:34 +0200

    fix import and move variable higher

--- tests/test_glsa.py
+++ tests/test_glsa.py
@@ -38,41 +38,42 @@ def test_glsa_test_system_affected():
 
 
 def test_rss():
-    from glsa import _rss
+    from pyerge.glsa import _rss
+    html = b'<?xml version="1.0" encoding="UTF-8"?>\n<rdf:RDF xmlns="http://purl.org/rss/1.0/"\n  ' \
+           b'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"\n  ' \
+           b'xmlns:content="http://purl.org/rss/1.0/modules/content/"\n  ' \
+           b'xmlns:dc="http://purl.org/dc/elements/1.1/"\n  ' \
+           b'xmlns:image="http://purl.org/rss/1.0/modules/image/"\n  ' \
+           b'xmlns:slash="http://purl.org/rss/1.0/modules/slash/"\n  ' \
+           b'xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"\n  ' \
+           b'xmlns:taxo="http://purl.org/rss/1.0/modules/taxonomy/"\n  ' \
+           b'xmlns:trackback="http://madskills.com/public/xml/rss/module/trackback/">\n' \
+           b'<channel rdf:about="https://security.gentoo.org/glsa">\n    ' \
+           b'<title>Gentoo Linux security advisories</title>\n    ' \
+           b'<link>https://security.gentoo.org/glsa</link>\n    ' \
+           b'<description>This feed contains new Gentoo Linux security advisories. ' \
+           b'Contact security@gentoo.org with questions.</description>\n' \
+           b'<items>\n      <rdf:Seq>\n' \
+           b'<rdf:li resource="https://security.gentoo.org/glsa/201904-25"/>\n' \
+           b'<rdf:li resource="https://security.gentoo.org/glsa/201904-24"/>\n' \
+           b'<rdf:li resource="https://security.gentoo.org/glsa/201904-23"/>\n' \
+           b'</items>\n    <dc:date>2019-04-24T00:00:00+00:00</dc:date>\n  </channel>\n' \
+           b'<item rdf:about="https://security.gentoo.org/glsa/201904-25">\n    ' \
+           b'<title>GLSA 201904-25: QEMU: Multiple vulnerabilities</title>\n    ' \
+           b'<link>https://security.gentoo.org/glsa/201904-25</link>\n    ' \
+           b'<dc:date>2019-04-24T00:00:00+00:00</dc:date>\n  </item>\n' \
+           b'<item rdf:about="https://security.gentoo.org/glsa/201904-24">\n    ' \
+           b'<title>GLSA 201904-24: Ming: Multiple vulnerabilities</title>\n    ' \
+           b'<link>https://security.gentoo.org/glsa/201904-24</link>\n    ' \
+           b'<dc:date>2019-04-24T00:00:00+00:00</dc:date>\n  </item>\n' \
+           b'<item rdf:about="https://security.gentoo.org/glsa/201904-23">\n    ' \
+           b'<title>GLSA 201904-23: GLib: Multiple vulnerabilities</title>\n    ' \
+           b'<link>https://security.gentoo.org/glsa/201904-23</link>\n    ' \
+           b'<dc:date>2019-04-22T00:00:00+00:00</dc:date>\n  </item>\n' \
+           b'</rdf:RDF>'
+
     with mock.patch('pyerge.glsa.urlopen') as urlopen_mock:
         with mock.patch('pyerge.glsa._collect_all_maching_entries') as collect_all_mock:
-            html = b'<?xml version="1.0" encoding="UTF-8"?>\n<rdf:RDF xmlns="http://purl.org/rss/1.0/"\n  ' \
-                   b'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"\n  ' \
-                   b'xmlns:content="http://purl.org/rss/1.0/modules/content/"\n  ' \
-                   b'xmlns:dc="http://purl.org/dc/elements/1.1/"\n  ' \
-                   b'xmlns:image="http://purl.org/rss/1.0/modules/image/"\n  ' \
-                   b'xmlns:slash="http://purl.org/rss/1.0/modules/slash/"\n  ' \
-                   b'xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"\n  ' \
-                   b'xmlns:taxo="http://purl.org/rss/1.0/modules/taxonomy/"\n  ' \
-                   b'xmlns:trackback="http://madskills.com/public/xml/rss/module/trackback/">\n' \
-                   b'<channel rdf:about="https://security.gentoo.org/glsa">\n    ' \
-                   b'<title>Gentoo Linux security advisories</title>\n    ' \
-                   b'<link>https://security.gentoo.org/glsa</link>\n    ' \
-                   b'<description>This feed contains new Gentoo Linux security advisories. ' \
-                   b'Contact security@gentoo.org with questions.</description>\n' \
-                   b'<items>\n      <rdf:Seq>\n' \
-                   b'<rdf:li resource="https://security.gentoo.org/glsa/201904-25"/>\n' \
-                   b'<rdf:li resource="https://security.gentoo.org/glsa/201904-24"/>\n' \
-                   b'<rdf:li resource="https://security.gentoo.org/glsa/201904-23"/>\n' \
-                   b'</items>\n    <dc:date>2019-04-24T00:00:00+00:00</dc:date>\n  </channel>\n' \
-                   b'<item rdf:about="https://security.gentoo.org/glsa/201904-25">\n    ' \
-                   b'<title>GLSA 201904-25: QEMU: Multiple vulnerabilities</title>\n    ' \
-                   b'<link>https://security.gentoo.org/glsa/201904-25</link>\n    ' \
-                   b'<dc:date>2019-04-24T00:00:00+00:00</dc:date>\n  </item>\n' \
-                   b'<item rdf:about="https://security.gentoo.org/glsa/201904-24">\n    ' \
-                   b'<title>GLSA 201904-24: Ming: Multiple vulnerabilities</title>\n    ' \
-                   b'<link>https://security.gentoo.org/glsa/201904-24</link>\n    ' \
-                   b'<dc:date>2019-04-24T00:00:00+00:00</dc:date>\n  </item>\n' \
-                   b'<item rdf:about="https://security.gentoo.org/glsa/201904-23">\n    ' \
-                   b'<title>GLSA 201904-23: GLib: Multiple vulnerabilities</title>\n    ' \
-                   b'<link>https://security.gentoo.org/glsa/201904-23</link>\n    ' \
-                   b'<dc:date>2019-04-22T00:00:00+00:00</dc:date>\n  </item>\n' \
-                   b'</rdf:RDF>'
             urlopen_mock.return_value.read.return_value = html
             collect_all_mock.return_value = ['201904-25', '201904-24', '201904-23']
             assert _rss(r'GLSA\s(\d{6}-\d{2}):\s.*', 2) == ['201904-25', '201904-24']
