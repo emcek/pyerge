a7d6c0a31efc7fff9e6595588d5f6159a02e45cc
Michal Plichta <michal.plichta@nokia.com>
2021-11-26 21:30:15 +0100

    move import

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,6 @@ from unittest import mock
 
 from pytest import mark
 
-from pyerge import utils
 
 PORT_TMP_DIR = '/var/tmp/portage'
 
@@ -10,10 +9,12 @@ PORT_TMP_DIR = '/var/tmp/portage'
 @mark.parametrize('size, result', [('12K', 12), ('34567k', 34567), ('31M', 31744), ('233m', 238592),
                                    ('2G', 2097152), ('1g', 1048576), ('500', 500), ('450.1', 450)])
 def test_convert2blocks(size, result):
+    from pyerge import utils
     assert utils.convert2blocks(size) == result
 
 
 def test_tmpfs_not_mounted():
+    from pyerge import utils
     with mock.patch('pyerge.utils.run_cmd') as run_cmd_mock:
         run_cmd_mock.return_value = b'/dev/sda2 on /boot type ext2 (rw,noatime,errors=continue,user_xattr,acl)\n' \
                                     b'rpc_pipefs on /var/lib/nfs/rpc_pipefs type rpc_pipefs (rw,relatime)\n' \
@@ -22,6 +23,7 @@ def test_tmpfs_not_mounted():
 
 
 def test_tmpfs_mounted():
+    from pyerge import utils
     with mock.patch('pyerge.utils.run_cmd') as run_cmd_mock:
         run_cmd_mock.return_value = b'/dev/sda2 on /boot type ext2 (rw,noatime,errors=continue,user_xattr,acl)\n' \
                                     b'rpc_pipefs on /var/lib/nfs/rpc_pipefs type rpc_pipefs (rw,relatime)\n' \
@@ -31,18 +33,21 @@ def test_tmpfs_mounted():
 
 
 def test_unmounttmpfs(opt_emerge_nonlocal_with_1g):
+    from pyerge import utils
     with mock.patch('pyerge.utils.run_cmd') as run_cmd_mock:
         utils.unmounttmpfs(opt_emerge_nonlocal_with_1g)
         run_cmd_mock.assert_called_once_with(f'sudo umount -f {PORT_TMP_DIR}')
 
 
 def test_mounttmpfs():
+    from pyerge import utils
     with mock.patch('pyerge.utils.run_cmd') as run_cmd_mock:
         utils.mounttmpfs(size='2G', verbose=2)
         run_cmd_mock.assert_called_once_with(f'sudo mount -t tmpfs -o size=2G,nr_inodes=1M tmpfs {PORT_TMP_DIR}')
 
 
 def test_remounttmpfs():
+    from pyerge import utils
     with mock.patch('pyerge.utils.run_cmd') as run_cmd_mock:
         utils.remounttmpfs(size='2G', verbose=2)
         run_cmd_mock.assert_has_calls([mock.call.run_cmd(f'sudo umount -f {PORT_TMP_DIR}'),
@@ -50,6 +55,7 @@ def test_remounttmpfs():
 
 
 def test_size_of_not_mounted_tmpfs():
+    from pyerge import utils
     with mock.patch('pyerge.utils.run_cmd') as run_cmd_mock:
         run_cmd_mock.return_value = b'Filesystem      1K-blocks     Used  Available Use% Mounted on\n' \
                                     b'/dev/sda2          126931    76647      43731  64% /boot\n' \
@@ -59,6 +65,7 @@ def test_size_of_not_mounted_tmpfs():
 
 
 def test_size_of_mounted_tmpfs():
+    from pyerge import utils
     with mock.patch('pyerge.utils.run_cmd') as run_cmd_mock:
         run_cmd_mock.return_value = b'Filesystem      1K-blocks     Used  Available Use% Mounted on\n' \
                                     b'/dev/sda2          126931    76647      43731  64% /boot\n' \
@@ -67,6 +74,7 @@ def test_size_of_mounted_tmpfs():
 
 
 def test_is_internet_connected():
+    from pyerge import utils
     with mock.patch('pyerge.utils.run_cmd') as run_cmd_mock:
         run_cmd_mock.return_value = b'PING 89.16.167.134 (89.16.167.134) 56(84) bytes of data.\n' \
                                     b'64 bytes from 89.16.167.134: icmp_seq=1 ttl=47 time=52.2 ms\n\n' \
@@ -77,18 +85,21 @@ def test_is_internet_connected():
 
 
 def test_is_internet_not_connected():
+    from pyerge import utils
     with mock.patch('pyerge.utils.run_cmd') as run_cmd_mock:
         run_cmd_mock.return_value = b'', b''
         assert utils.is_internet_connected(verbose=2) is False
 
 
 def test_delete_content():
+    from pyerge import utils
     with mock.patch('pyerge.utils.open') as open_mock:
         utils.delete_content(fname='/tmp/emerge.log')
         open_mock.assert_called_once_with(file='/tmp/emerge.log', mode='w', encoding='utf-8')
 
 
 def test_run_cmd_as_subprocess_ver1():
+    from pyerge import utils
     with mock.patch('pyerge.utils.Popen') as popen_mock:
         process_mock = mock.Mock()
         attrs = {'communicate.return_value': (b'Filesystem      1K-blocks     Used  Available Use% Mounted on\n'
@@ -101,6 +112,7 @@ def test_run_cmd_as_subprocess_ver1():
 
 
 def test_run_cmd_as_subprocess_ver2():
+    from pyerge import utils
     from subprocess import Popen
     with mock.patch.object(Popen, 'communicate') as communicate_mock:
         out = b'Filesystem      1K-blocks     Used  Available Use% Mounted on\n' \
@@ -112,6 +124,7 @@ def test_run_cmd_as_subprocess_ver2():
 
 
 def test_run_cmd_as_sysyem():
+    from pyerge import utils
     with mock.patch('pyerge.utils.system') as system_mock:
         system_mock.return_value = 0
         assert utils.run_cmd(cmd='df', use_system=True) == (b'0', b'')
@@ -120,17 +133,20 @@ def test_run_cmd_as_sysyem():
 
 def test_set_portage_tmpdir(monkeypatch):
     from os import environ
+    from pyerge import utils
     monkeypatch.setitem(environ, 'PORTAGE_TMPDIR', '')
     assert utils.set_portage_tmpdir() == '/var/tmp/portage'
 
 
 def test_portage_tmpdir_already_set(monkeypatch):
     from os import environ
+    from pyerge import utils
     monkeypatch.setitem(environ, 'PORTAGE_TMPDIR', 'some_value')
     assert utils.set_portage_tmpdir() == 'some_value'
 
 
 def test_handling_mounting_mount(opt_emerge_nonlocal_with_1g):
+    from pyerge import utils
     with mock.patch('pyerge.utils.mounttmpfs') as mounttmpfs_mock:
         with mock.patch('pyerge.utils.is_tmpfs_mounted') as is_tmpfs_mounted_mock:
             is_tmpfs_mounted_mock.return_value = False
@@ -139,6 +155,7 @@ def test_handling_mounting_mount(opt_emerge_nonlocal_with_1g):
 
 
 def test_handling_mounting_remounte(opt_emerge_nonlocal_with_1g):
+    from pyerge import utils
     with mock.patch('pyerge.utils.size_of_mounted_tmpfs') as size_of_mounted_tmpfs_mock:
         with mock.patch('pyerge.utils.convert2blocks') as convert2blocks_mock:
             with mock.patch('pyerge.utils.is_tmpfs_mounted') as is_tmpfs_mounted_mock:
@@ -149,6 +166,7 @@ def test_handling_mounting_remounte(opt_emerge_nonlocal_with_1g):
 
 
 def test_handling_mounting_else(opt_emerge_nonlocal_with_1g):
+    from pyerge import utils
     with mock.patch('pyerge.utils.size_of_mounted_tmpfs') as size_of_mounted_tmpfs_mock:
         with mock.patch('pyerge.utils.convert2blocks') as convert2blocks_mock:
             with mock.patch('pyerge.utils.is_tmpfs_mounted') as is_tmpfs_mounted_mock:
